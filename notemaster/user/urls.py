888724edf3e615c68749e9c04662addcb0df2b88
Aashu <prajapatiaashu4444@gmail.com>
2022-01-24 10:02:30 +0530

    Added new router - '/api/user/delete/'

--- notemaster/user/urls.py
+++ notemaster/user/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('get/', views.getUser, name="getUser"),
     path('create/', views.create_user, name="create_user"),
     path('login/', views.login_user, name="login_user"),
+    path('delete/', views.delete_user, name="delete_user"),
 ]
