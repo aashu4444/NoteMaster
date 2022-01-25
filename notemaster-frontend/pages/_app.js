f96d389d0ee348524e1e526065621839befb047f
Aashu <prajapatiaashu4444@gmail.com>
2022-01-25 10:22:27 +0530

    Update : Added meta tags and favicon

--- notemaster-frontend/pages/_app.js
+++ notemaster-frontend/pages/_app.js
@@ -4,6 +4,7 @@ import { GlobalState } from '../context/GlobalContext';
 import { ThemeProvider } from 'next-themes';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -14,7 +15,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const authToken = localStorage.getItem("authToken");
 
-    if (authToken===null || authToken==="") {
+    if (authToken === null || authToken === "") {
       for (let url of authenticatedUrls) {
         if (router.pathname === url) {
           router.push("/login");
@@ -26,6 +27,17 @@ function MyApp({ Component, pageProps }) {
 
   return <ThemeProvider attribute="class">
     <GlobalState>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="robots" content="all" />
+        <meta
+          name="description"
+          content="NoteMaster helps you to save your notes on the cloud and it is easy to manage your notes on NoteMaster. You can create labels and categorize your images easily. Why you are waiting for? create your account and start using NoteMaster now, this is quick and easy!."
+        />
+
+      </Head>
       <Component {...pageProps} />
     </GlobalState>
   </ThemeProvider>
