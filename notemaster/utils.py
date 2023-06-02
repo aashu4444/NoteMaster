5e172d9ed6d5f633901d440a05d956b2b4274431
Ashutosh <prajapatiaashu4444@gmail.com>
2023-06-02 13:18:28 +0530

    Update:Connect to Atlas database when not in debug mode

--- notemaster/utils.py
+++ notemaster/utils.py
@@ -1,14 +1,37 @@
 from venv import create
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 import jwt
 from notemaster.settings import SECRET_KEY
 import hashlib
+from secrets import DB_PASSWORD
+from notemaster.settings import DEBUG
 
 def get_db():
-    client = MongoClient('localhost', 27017)
-    db = client["notemaster"]
+    client = None
+    if DEBUG == True:
+        uri = "mongodb+srv://ashu:" + DB_PASSWORD + "@cluster0.3ofil.mongodb.net/?retryWrites=true&w=majority"
+
+        # Create a new client and connect to the server
+        client = MongoClient(uri, server_api=ServerApi('1'))
+
+        # Send a ping to confirm a successful connection
+        try:
+            client.admin.command('ping')
+            db = client["notemaster"]
+
+            return db
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+
+        except Exception as e:
+            
+            return "Unable to connect to databse"
+
+    else:
+        client = MongoClient('localhost', 27017)
+        db = client["notemaster"]
     
-    return db
+        return db
 
 def loggedin(request):
     authToken = request.headers.get("authToken")
